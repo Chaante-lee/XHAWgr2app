77104780ef1bb22df6c74f9b3d9897114fa63420
Chaante' Lee <ST10481788@myemeris.edu.za>
2026-04-08 17:02:29 +0200

    new commit

--- app/src/main/java/com/example/group2app/MainActivity.kt
+++ app/src/main/java/com/example/group2app/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.group2app
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val navHome = findViewById<TextView>(R.id.navHome)
+        val navCourses = findViewById<TextView>(R.id.navCourses)
+        val navQuote = findViewById<TextView>(R.id.navQuote)
+        val navContact = findViewById<TextView>(R.id.navContact)
+        val btnQuote = findViewById<Button>(R.id.btnQuote)
+
+        // Navigation clicks
+        navHome.setOnClickListener {
+            // Already on home
+        }
+
+        navCourses.setOnClickListener {
+            startActivity(Intent(this, CoursesActivity::class.java))
+        }
+
+        navQuote.setOnClickListener {
+            startActivity(Intent(this, QuoteActivity::class.java))
+        }
+
+        navContact.setOnClickListener {
+            startActivity(Intent(this, ContactActivity::class.java))
+        }
+
+        btnQuote.setOnClickListener {
+            startActivity(Intent(this, QuoteActivity::class.java))
         }
     }
 }
